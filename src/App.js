f0325d1871db357c0fcacbce50340cc2eb43148c
Jules Sannequin <jules.sannequin@gmail.com>
2022-12-23 16:31:31 +0100

    Starting to code the frontend in React with an axios request to fetch the dataof the backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+// const config = {
+//   headers: {
+//     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
+//     "Access-Control-Allow-Origin": "https://localhost:3001/",
+//     "Content-Type": "application/json",
+//   },
+// };
 
 function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setisLoading] = useState(true);
+
+  try {
+  } catch (error) {}
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await axios.get("http://localhost:3000/");
+        setData(response.data);
+        console.log(response.data);
+        setisLoading(false);
+      };
+      fetchData();
+    } catch (error) {
+      console.log(error.response);
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
+
+// function App() {
+//   useEffect(() => {
+//     axios.get("http://localhost:3000/", config).then((response) => {
+//       console.log(response.data);
+//     });
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <h2>Hello world</h2>
+//     </div>
+//   );
+// }
 
 export default App;
